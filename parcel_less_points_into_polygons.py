f101a5c594a8f73c54f615b7bee2ba43f9b92a8c
WenkChr <christopher.wenkoff@canada.ca>
2021-04-13 11:28:13 -0400

    Deleted explode function

--- parcel_less_points_into_polygons.py
+++ parcel_less_points_into_polygons.py
@@ -24,23 +24,6 @@ to the building fooprints.
 # ------------------------------------------------------------------------------------------------------------
 # Functions
 
-def explode(ingdf): 
-    # not one of Jesse's. To solve multipolygon issue
-    indf = ingdf
-    outdf = gpd.GeoDataFrame(columns=indf.columns)
-    for idx, row in indf.iterrows():
-        
-        if type(row.geometry) == Polygon:
-            outdf = outdf.append(row,ignore_index=True)
-        if type(row.geometry) == MultiPolygon:
-            multdf = gpd.GeoDataFrame(columns=indf.columns)
-            recs = len(row.geometry)
-            multdf = multdf.append([row]*recs,ignore_index=True)
-            for geom in range(recs):
-                multdf.loc[geom,'geometry'] = row.geometry[geom]
-            outdf = outdf.append(multdf,ignore_index=True)
-    return outdf
-
 def list_bf_indexes(buffer_geom, bf_gdf):
     """
     For parcel-less bf geometry takes the buffer from the buffer_geom field and looks for 
