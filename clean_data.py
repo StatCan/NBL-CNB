734b9ed8b13b44a43cc50ae5acd081e6b88e5c78
WenkChr <christopher.wenkoff@canada.ca>
2021-04-16 16:42:02 -0400

    changed export file type. Removed issues causing cleaning code until fox can be found

--- clean_data.py
+++ clean_data.py
@@ -42,7 +42,6 @@ output_path = r'H:\point_to_polygon_PoC\data\workingfiles'
 project_gpkg = "H:/point_to_polygon_PoC/data/data.gpkg"
 addresses_lyr_nme = "yk_Address_Points"
 bf_lyr_nme = "yk_buildings_sj"
-bf_polys = "yk_buildings_sj"
 
 # ------------------------------------------------------------------------------------------------
 # Logic
@@ -53,20 +52,19 @@ footprint = gpd.read_file(project_gpkg, layer= bf_lyr_nme)
   
 
 print('Cleaning address points')
-
 addresses = addresses[(addresses.CIVIC_ADDRESS != "RITE OF WAY")]
 addresses = reproject(addresses, 26911)
+
 print('Cleaning building footprints')
 # Remove blanks and nulls
-footprint = footprint[(footprint.Join_Count > 0) & (footprint.STREET_NAME.notnull()) & (footprint.STREET_NAME != ' ')] 
+# footprint = footprint[(footprint.Join_Count > 0) & (footprint.STREET_NAME.notnull()) & (footprint.STREET_NAME != ' ')] # This causes missing intersects 224 cases should be triages into the nonlinking instead
 footprint = explode(footprint) # Remove multipart polygons convert to single polygons
 footprint['area'] = footprint['geometry'].area
 footprint = footprint.loc[footprint.area >= 20.0] # Remove all buildings with an area of less than 20m**2
 footprint = reproject(footprint, 26911)
 print('Exporting cleaned datasets')
 
-print(footprint.crs)
-footprint.to_file(os.path.join(output_path, 'footprints_cleaned.geojson'), driver='GeoJSON')
-addresses.to_file(os.path.join(output_path, 'addresses_cleaned.geojson'), driver='GeoJSON')
+footprint.to_file(project_gpkg, layer='footprints_cleaned', driver='GPKG')
+addresses.to_file(project_gpkg, layer='addresses_cleaned', driver='GPKG')
 
 print('DONE!')
