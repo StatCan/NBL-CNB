786d97b0fa5eeb078d40d6813abc6f2105dcca9b
WenkChr <christopher.wenkoff@canada.ca>
2021-04-27 14:33:01 -0400

    added spatial join for the buildings and linking data. Added creation of a linking datafield that will be unique for each parcel

--- clean_data.py
+++ clean_data.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 import pandas as pd
 import re
-
+import sys
 # ------------------------------------------------------------------------------------------------
 # Functions
 
@@ -39,17 +39,27 @@ def reproject(ingdf, output_crs):
 output_path = r'H:\point_to_polygon_PoC\data\workingfiles'
 
 # Layer inputs
+footprint_gdb = r'H:\point_to_polygon_PoC\data\yk_buildings_gdb.gdb'
+footprint_lyr_nme = 'yk_buildings'
+
+ap_gdb = r'H:\point_to_polygon_PoC\data\yk_AddressPoints_gdb.gdb'
+ap_lyr_nme = 'yk_Address_Points'
+
+linking_gdb = r'H:\point_to_polygon_PoC\data\yk_parcels_gdb.gdb'
+linking_lyr_nme = 'yk_parcels' 
+
+# output gpkg
 project_gpkg = "H:/point_to_polygon_PoC/data/data.gpkg"
-addresses_lyr_nme = "yk_Address_Points"
-bf_lyr_nme = "yk_buildings_sj"
 
 # ------------------------------------------------------------------------------------------------
 # Logic
 
 # Load dataframes.
-addresses = gpd.read_file(project_gpkg, layer= addresses_lyr_nme)
-footprint = gpd.read_file(project_gpkg, layer= bf_lyr_nme)
-  
+print('Loading in data')
+addresses = gpd.read_file(ap_gdb, layer=ap_lyr_nme)
+footprint = gpd.read_file(footprint_gdb, layer=footprint_lyr_nme)
+
+linking_data = gpd.read_file(linking_gdb, layer= linking_lyr_nme, crs=26911)
 
 print('Cleaning address points')
 addresses = addresses[(addresses.CIVIC_ADDRESS != "RITE OF WAY")]
@@ -62,8 +72,14 @@ footprint = explode(footprint) # Remove multipart polygons convert to single pol
 footprint['area'] = footprint['geometry'].area
 footprint = footprint.loc[footprint.area >= 20.0] # Remove all buildings with an area of less than 20m**2
 footprint = reproject(footprint, 26911)
-print('Exporting cleaned datasets')
+footprint = gpd.sjoin(footprint, linking_data)
+footprint.drop(columns=['index_right'], inplace=True)
+print(footprint.head())
+print('Creating linking data field')
+footprint['link_field'] = footprint.apply(lambda x: '%s_%s_%s' % (x['LOT'], x['BLOCK'], x['PLAN_']), axis=1)
+addresses['link_field'] = addresses.apply(lambda x: '%s_%s_%s' % (x['LOT'], x['BLOCK'], x['PLAN_']), axis=1)
 
+print('Exporting cleaned datasets')
 footprint.to_file(project_gpkg, layer='footprints_cleaned', driver='GPKG')
 addresses.to_file(project_gpkg, layer='addresses_cleaned', driver='GPKG')
 
