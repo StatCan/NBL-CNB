985e9b6288413289d81386eb1a3769e86ae404aa
WenkChr <christopher.wenkoff@canada.ca>
2021-04-30 21:07:29 -0400

    Added in dotenv vars. Changed how linking field is made to be more versatile for future runs. Added mask due to the limited scope of the building data as compared to the size of the province.

--- clean_data.py
+++ clean_data.py
@@ -1,4 +1,6 @@
 from shapely import geometry
+from dotenv import load_dotenv
+from pathlib import Path
 from shapely.geometry import Point, Polygon, MultiPolygon
 import geopandas as gpd
 import numpy as np
@@ -35,49 +37,59 @@ def reproject(ingdf, output_crs):
     return ingdf
 # ------------------------------------------------------------------------------------------------
 # Inputs
-
-output_path = r'H:\point_to_polygon_PoC\data\workingfiles'
+load_dotenv(os.path.join(os.getcwd(), 'environments.env'))
 
 # Layer inputs
-footprint_gdb = r'H:\point_to_polygon_PoC\data\yk_buildings_gdb.gdb'
-footprint_lyr_nme = 'yk_buildings'
+proj_crs = os.getenv('BC_CRS')
 
-ap_gdb = r'H:\point_to_polygon_PoC\data\yk_AddressPoints_gdb.gdb'
-ap_lyr_nme = 'yk_Address_Points'
+footprint_lyr = Path(os.getenv('BC_BF_PATH'))
 
-linking_gdb = r'H:\point_to_polygon_PoC\data\yk_parcels_gdb.gdb'
-linking_lyr_nme = 'yk_parcels' 
+ap_path = Path(os.getenv('BC_ADDRESS_PATH'))
+# ap_lyr_nme = os.getenv('BC_ADDRESS_LYR_NME')
+
+linking_gdb = Path(os.getenv('BC_LINKING_PATH'))
+linking_lyr_nme = os.getenv('BC_LINKING_LYR_NME')
+linking_ignore_columns = os.getenv('BC_LINKING_IGNORE_COLS') 
+
+# AOI mask if necessary
+aoi_mask = os.getenv('BC_ODB_MASK')
 
 # output gpkg
-project_gpkg = "H:/point_to_polygon_PoC/data/data.gpkg"
+project_gpkg = Path(os.getenv('OUTPUT_GPKG'))
 
 # ------------------------------------------------------------------------------------------------
 # Logic
 
 # Load dataframes.
-print('Loading in data')
-addresses = gpd.read_file(ap_gdb, layer=ap_lyr_nme)
-footprint = gpd.read_file(footprint_gdb, layer=footprint_lyr_nme)
+if len(aoi_mask) > 0 or type(aoi_mask) == None:
+    aoi_gdf = gpd.read_file(aoi_mask)
 
-linking_data = gpd.read_file(linking_gdb, layer= linking_lyr_nme, crs=26911)
+print('Loading in address data')
+addresses = gpd.read_file(ap_path, mask=aoi_gdf)
+print('Loading in footprint data')
+footprint = gpd.read_file(footprint_lyr, mask=aoi_gdf)
+print('loading in linking data')
+linking_data = gpd.read_file(linking_gdb, layer= linking_lyr_nme, linking_ignore_columns= linking_ignore_columns)
+linking_data['link_field'] = range(1, len(linking_data.index)+1)
+linking_data = reproject(linking_data, proj_crs, mask=aoi_gdf)
 
 print('Cleaning address points')
-addresses = addresses[(addresses.CIVIC_ADDRESS != "RITE OF WAY")]
-addresses = reproject(addresses, 26911)
-
+# addresses = addresses[(addresses.CIVIC_ADDRESS != "RITE OF WAY")]
+addresses = reproject(addresses, proj_crs)
+addresses = gpd.sjoin(addresses, linking_data)
 print('Cleaning building footprints')
 # Remove blanks and nulls
 # footprint = footprint[(footprint.Join_Count > 0) & (footprint.STREET_NAME.notnull()) & (footprint.STREET_NAME != ' ')] # This causes missing intersects 224 cases should be triages into the nonlinking instead
 footprint = explode(footprint) # Remove multipart polygons convert to single polygons
 footprint['area'] = footprint['geometry'].area
 footprint = footprint.loc[footprint.area >= 20.0] # Remove all buildings with an area of less than 20m**2
-footprint = reproject(footprint, 26911)
+footprint = reproject(footprint, proj_crs)
 footprint = gpd.sjoin(footprint, linking_data)
 footprint.drop(columns=['index_right'], inplace=True)
-print(footprint.head())
-print('Creating linking data field')
-footprint['link_field'] = footprint.apply(lambda x: '%s_%s_%s' % (x['LOT'], x['BLOCK'], x['PLAN_']), axis=1)
-addresses['link_field'] = addresses.apply(lambda x: '%s_%s_%s' % (x['LOT'], x['BLOCK'], x['PLAN_']), axis=1)
+
+# print('Creating linking data field')
+# footprint['link_field'] = footprint.apply(lambda x: '%s_%s_%s' % (x['LOT'], x['BLOCK'], x['PLAN_']), axis=1)
+# addresses['link_field'] = addresses.apply(lambda x: '%s_%s_%s' % (x['LOT'], x['BLOCK'], x['PLAN_']), axis=1)
 
 print('Exporting cleaned datasets')
 footprint.to_file(project_gpkg, layer='footprints_cleaned', driver='GPKG')
