b5534768d49e4cf281702eef3ff7fafaf165d3e1
WenkChr <christopher.wenkoff@canada.ca>
2021-05-12 14:31:33 -0400

    changes to vars to accept NT data, changes to cleaning process for footprints and adp. Added linking data sjoins to add linking fields to the data

--- clean_data.py
+++ clean_data.py
@@ -1,13 +1,15 @@
-from shapely import geometry
-from dotenv import load_dotenv
-from pathlib import Path
-from shapely.geometry import Point, Polygon, MultiPolygon
-import geopandas as gpd
-import numpy as np
 import os
-import pandas as pd
 import re
 import sys
+from pathlib import Path
+
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+from dotenv import load_dotenv
+from shapely import geometry
+from shapely.geometry import MultiPolygon, Point, Polygon
+
 # ------------------------------------------------------------------------------------------------
 # Functions
 
@@ -40,59 +42,78 @@ def reproject(ingdf, output_crs):
 load_dotenv(os.path.join(os.getcwd(), 'environments.env'))
 
 # Layer inputs
-proj_crs = os.getenv('BC_CRS')
+proj_crs = os.getenv('NT_CRS')
 
-footprint_lyr = Path(os.getenv('BC_BF_PATH'))
+footprint_lyr = Path(os.getenv('NT_BF_PATH'))
 
-ap_path = Path(os.getenv('BC_ADDRESS_PATH'))
+ap_path = Path(os.getenv('NT_ADDRESS_PATH'))
 # ap_lyr_nme = os.getenv('BC_ADDRESS_LYR_NME')
+ap_add_fields = ['street_no', 'street', 'geometry']
 
-linking_gdb = Path(os.getenv('BC_LINKING_PATH'))
-linking_lyr_nme = os.getenv('BC_LINKING_LYR_NME')
-linking_ignore_columns = os.getenv('BC_LINKING_IGNORE_COLS') 
+linking_data_path = Path(os.getenv('NT_LINKING_PATH'))
+# linking_lyr_nme = os.getenv('BC_LINKING_LYR_NME')
+linking_ignore_columns = os.getenv('NT_LINKING_IGNORE_COLS') 
 
 # AOI mask if necessary
-aoi_mask = os.getenv('BC_ODB_MASK')
+aoi_mask = os.getenv('NT_ODB_MASK')
 
 # output gpkg
-project_gpkg = Path(os.getenv('OUTPUT_GPKG'))
+project_gpkg = Path(os.getenv('NT_GPKG'))
 
 # ------------------------------------------------------------------------------------------------
 # Logic
 
 # Load dataframes.
-if len(aoi_mask) > 0 or type(aoi_mask) == None:
-    aoi_gdf = gpd.read_file(aoi_mask)
+# if type(aoi_mask) != None:
+#     aoi_gdf = gpd.read_file(aoi_mask)
+
+# aoi_gdf = aoi_gdf.loc[aoi_gdf['CSD_UID'] == '5915022']
+
+print('loading in linking data')
+linking_data = gpd.read_file(linking_data_path, linking_ignore_columns=linking_ignore_columns) # mask=aoi_gdf)
+linking_cols_drop = linking_data.columns.tolist()
+linking_data['link_field'] = range(1, len(linking_data.index)+1)
+linking_data = reproject(linking_data, proj_crs)
+linking_cols_drop.remove('geometry')
+linking_cols_drop += ['index_right']
 
 print('Loading in address data')
-addresses = gpd.read_file(ap_path, mask=aoi_gdf)
-print('Loading in footprint data')
-footprint = gpd.read_file(footprint_lyr, mask=aoi_gdf)
-print('loading in linking data')
-linking_data = gpd.read_file(linking_gdb, layer= linking_lyr_nme, linking_ignore_columns= linking_ignore_columns)
-linking_data['link_field'] = range(1, len(linking_data.index)+1)
-linking_data = reproject(linking_data, proj_crs, mask=aoi_gdf)
+if os.path.split(ap_path)[-1].endswith('.csv'):
+    addresses = pd.read_csv(ap_path)
+    addresses = gpd.GeoDataFrame(addresses, geometry=gpd.points_from_xy(addresses.longitude, addresses.latitude))
+else:
+    addresses = gpd.read_file(ap_path) #, mask=aoi_gdf)
 
 print('Cleaning address points')
-# addresses = addresses[(addresses.CIVIC_ADDRESS != "RITE OF WAY")]
+
+addresses = addresses[ap_add_fields]
 addresses = reproject(addresses, proj_crs)
-addresses = gpd.sjoin(addresses, linking_data)
+addresses = gpd.sjoin(addresses, linking_data, op='within')
+addresses.drop(columns=linking_cols_drop, inplace=True)
+for f in ['index_right', 'index_left']:
+    if f in addresses.columns.tolist():
+        addresses.drop(columns=f, inplace=True)
+
+print('Exporting cleaned dataset')
+addresses.to_file(project_gpkg, layer='addresses_cleaned', driver='GPKG')
+del addresses
+
+print('Loading in footprint data')
+footprint = gpd.read_file(footprint_lyr)# , mask=aoi_gdf)
+
 print('Cleaning building footprints')
-# Remove blanks and nulls
-# footprint = footprint[(footprint.Join_Count > 0) & (footprint.STREET_NAME.notnull()) & (footprint.STREET_NAME != ' ')] # This causes missing intersects 224 cases should be triages into the nonlinking instead
-footprint = explode(footprint) # Remove multipart polygons convert to single polygons
+# footprint = explode(footprint) # Remove multipart polygons convert to single polygons
 footprint['area'] = footprint['geometry'].area
 footprint = footprint.loc[footprint.area >= 20.0] # Remove all buildings with an area of less than 20m**2
 footprint = reproject(footprint, proj_crs)
-footprint = gpd.sjoin(footprint, linking_data)
-footprint.drop(columns=['index_right'], inplace=True)
+footprint = gpd.sjoin(footprint, linking_data, how='left', op='within')
+footprint.drop(columns=linking_cols_drop, inplace=True)
 
-# print('Creating linking data field')
-# footprint['link_field'] = footprint.apply(lambda x: '%s_%s_%s' % (x['LOT'], x['BLOCK'], x['PLAN_']), axis=1)
-# addresses['link_field'] = addresses.apply(lambda x: '%s_%s_%s' % (x['LOT'], x['BLOCK'], x['PLAN_']), axis=1)
+for f in ['index_right', 'index_left']:
+    if f in footprint.columns.tolist():
+        footprint.drop(columns=f, inplace=True)
 
-print('Exporting cleaned datasets')
+print('Exporting cleaned dataset')
 footprint.to_file(project_gpkg, layer='footprints_cleaned', driver='GPKG')
-addresses.to_file(project_gpkg, layer='addresses_cleaned', driver='GPKG')
 
 print('DONE!')
