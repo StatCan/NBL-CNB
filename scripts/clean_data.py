e579bc44c17b07d3739f51fd74ae26686e94f761
WenkChr <christopher.wenkoff@canada.ca>
2021-06-12 10:34:07 -0400

    Additional road cleaning road type standardization for comparision purposes

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -52,6 +52,33 @@ def records(filename, usecols, **kwargs):
             yield f
 
 
+def str_type_cln(street_name, correction_dict):
+    '''Cleans the street types so that they are standardized across all datasets returns the corrected street type in upper case'''
+    street_name = street_name.upper()
+    street_split = street_name.split()
+    
+    # If single word or empty return the name as is
+    if (street_split == 1 or street_split == 0):
+        return street_name
+
+    s_type = street_split[-1]
+    type_index = street_split.index(s_type)
+    
+    # If last word is a direction take the second last word
+    if (len(s_type) == 1 or len(s_type) == 2) and (s_type in ['N', 'S', 'E', 'W', 'NE', 'NW', 'SE', 'SW', 'O', 'SO']):
+        s_type = street_split[-2]
+        type_index = street_split.index(s_type)
+    
+    # ensure that street type conforms to the correct appreviation as outlined in the list
+    if s_type in correction_dict:
+        s_type = correction_dict[s_type]
+    
+    #Put the cleaned street name back together
+    street_split[type_index] = s_type
+    cleaned_name = " ".join(street_split)
+    
+    return cleaned_name
+    
 # ------------------------------------------------------------------------------------------------
 # Inputs
 load_dotenv(os.path.join(os.path.dirname(__file__), 'environments.env'))
@@ -71,7 +98,7 @@ linking_ignore_columns = os.getenv('NT_LINKING_IGNORE_COLS')
 
 rd_gpkg = Path(os.getenv('NT_RD_GPKG'))
 rd_lyr_nme = os.getenv('NT_RD_LYR_NME')
-rd_use_flds = ['L_HNUMF', 'R_HNUMF', 'L_HNUML', 'R_HNUML', 'L_STNAME_C', 'R_STNAME_C']
+rd_use_flds = ['L_HNUMF', 'R_HNUMF', 'L_HNUML', 'R_HNUML', 'L_STNAME_C', 'R_STNAME_C', 'ROADCLASS']
 # AOI mask if necessary
 aoi_mask = os.getenv('NT_ODB_MASK')
 
@@ -79,6 +106,41 @@ aoi_mask = os.getenv('NT_ODB_MASK')
 project_gpkg = Path(os.getenv('NT_GPKG'))
 rd_crs = os.getenv('NT_RD_CRS')
 
+# Road name correction dictionary
+type_corr_dict = {
+    'CR' : 'CRES',
+    'LN' : 'LANE', 
+    'CRESCENT': 'CRES', 
+    'AVENUE' : 'AVE',
+    'BOULEVARD' : 'BLVD',
+    'DRIVE' : 'DR', 
+    'LANE' : 'LN', 
+    'PLACE' : 'PL',
+    'CENTRE' : 'CTR',
+    'CIRCLE' : 'CIR', 
+    'CIRCUIT' : 'CIRCT',
+    'CHEMIN' : 'CH', 
+    'CONCESSION' : 'CONC',
+    'COURT' : 'CRT',
+    'DRIVE' : 'DR',
+    'HIGHWAY' : 'HWY',
+    'PLACE' : 'PL',
+    'POINT' : 'PT',
+    'PRIVATE' : 'PVT',
+    'PROMINADE' : 'PROM',
+    'RANGE' : 'RG',
+    'ROAD' :'RD',
+    'ROUTE' : 'RTE',
+    'STREET' : 'ST',
+    'SUBDIVISION' : 'SUB',
+    'TERRACE' : 'TSSE',
+}
+master_types = ['ABBEY', 'ACRES', 'ALLÉE', 'ALLEY', 'AUT', 'AVE', 'AV', 'BAY', 'BEACH', 'BEND', 'BLVD', 'BOUL', 'BYPASS', 'BYWAY', 'CAMPUS', 'CAPE', 'CAR', 
+            'CARREF', 'CTR', 'C', 'CERCLE', 'CHASE', 'CH', 'CIR', 'CIRCT', 'CLOSE', 'COMMON', 'CONC', 'CRNRS', 'CÔTE', 'COUR', 'COURS', 'CRT', 'COVE', 'CRES', 
+            'CROIS', 'CROSS', 'CDS', 'DALE', 'DELL', 'DIVERS', 'DOWNS', 'DR', 'ÉCH', 'END', 'ESPL', 'ESTATE', 'EXPY', 'EXTEN', 'PINES','PL', 'PLACE', 'PLAT', 
+            'PLAZA', 'PT', 'POINTE', 'PORT', 'PVT', 'PROM', 'QUAI', 'QUAY', 'RAMP', 'RANG', 'RG', 'RIDGE', 'RISE', 'RD', 'RDPT', 'RTE', 'ROW', 'RUE', 'RLE', 
+            'RUN', 'SENT', 'SQ', 'ST', 'SUBDIV', 'TERR', 'TSSE', 'THICK', 'TOWERS', 'TLINE', 'TRAIL', 'TRNABT', 'VALE', 'VIA', 'VIEW', 'VILLGE', 'VILLAS', 
+            'VISTA', 'VOIE', 'WALK', 'WAY', 'WHARF', 'WOOD', 'WYND']
 # ------------------------------------------------------------------------------------------------
 # Logic
 
@@ -116,7 +178,7 @@ for f in ['index_right', 'index_left']:
 addresses = addresses[addresses["street_no"] != 'RITE OF WAY']
 addresses["suffix"] = addresses["street_no"].map(lambda val: re.sub(pattern="\\d+", repl="", string=val, flags=re.I))
 addresses["number"] = addresses["street_no"].map(lambda val: re.sub(pattern="[^\\d]", repl="", string=val, flags=re.I)).map(int)
-
+addresses['street'] = addresses['street'].str.upper()
 print('Exporting cleaned dataset')
 addresses.to_file(project_gpkg, layer='addresses_cleaned', driver='GPKG')
 del addresses
@@ -126,9 +188,22 @@ roads = gpd.GeoDataFrame.from_features(records(rd_gpkg, rd_use_flds, layer=rd_ly
 roads.set_crs(epsg=rd_crs, inplace=True)
 
 print('Cleaning and prepping road data')
+# Clean Roads to match the address format
+# Remove all the unknown named roads
+roads = roads[(roads['L_STNAME_C'] != 'Unknown') | (roads['R_STNAME_C'] != 'Unknown')]
+# Remove all the winder roads
+roads = roads[(roads['ROADCLASS'] != 'Winter')]
+
+# Remove additional punctuation that is no longer needed
 roads['l_nme_cln'] = roads.L_STNAME_C.str.replace('[^\w\s-]', '')
 roads['r_nme_cln'] = roads.R_STNAME_C.str.replace('[^\w\s-]', '')
+
+# Correct road type abbreviations
+roads['l_nme_cln'] = roads['l_nme_cln'].apply(lambda row: str_type_cln(row, type_corr_dict))
+
+# Drop unecessary columns
 roads.drop(columns=['L_STNAME_C', 'R_STNAME_C'],  inplace=True)
+
 print('Exporting cleaned dataset')
 roads.to_file(project_gpkg, layer='roads_cleaned', driver='GPKG')
 del roads
@@ -162,3 +237,7 @@ print('Exporting cleaned dataset')
 footprint.to_file(project_gpkg, layer='footprints_cleaned', driver='GPKG')
 
 print('DONE!')
+[
+    'ABBEY', 'ACRES', 'ALLÉE', 'ALLEY', 'AUT', 'AVE', 'AV', 'BAY', 'BEACH', 'BEND', 'BLVD', 'BOUL', 'BYPASS', 'BYWAY', 'CAMPUS', 'CAPE', 'CAR', 'CARREF', 'CTR', 'C', 'CERCLE', 'CHASE', 'CH', 'CIR', 'CIRCT', 'CLOSE', 'COMMON', 'CONC', 'CRNRS', 'CÔTE', 'COUR', 'COURS', 'CRT', 'COVE', 'CRES', 'CROIS', 'CROSS', 'CDS', 'DALE', 'DELL', 'DIVERS', 'DOWNS', 'DR', 'ÉCH', 'END', 'ESPL', 'ESTATE', 'EXPY', 'EXTEN',
+    'PINES','PL', 'PLACE', 'PLAT', 'PLAZA', 'PT', 'POINTE', 'PORT', 'PVT', 'PROM', 'QUAI', 'QUAY', 'RAMP', 'RANG', 'RG', 'RIDGE', 'RISE', 'RD', 'RDPT', 'RTE', 'ROW', 'RUE', 'RLE', 'RUN', 'SENT', 'SQ', 'ST', 'SUBDIV', 'TERR', 'TSSE', 'THICK', 'TOWERS', 'TLINE', 'TRAIL', 'TRNABT', 'VALE', 'VIA', 'VIEW', 'VILLGE', 'VILLAS', 'VISTA', 'VOIE', 'WALK', 'WAY', 'WHARF', 'WOOD', 'WYND'
+]
