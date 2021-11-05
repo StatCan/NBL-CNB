d1571b1287e563c0cc1d84d0527c95d9ca2a9ddc
WenkChr <christopher.wenkoff@canada.ca>
2021-11-05 15:19:02 -0400

    added code to set the CRS for each layer fixed the 0 matches issues. Still takes a long to time to assess match quality

--- scripts/issue_flagging.py
+++ scripts/issue_flagging.py
@@ -1,5 +1,6 @@
 import os, sys
 import pandas as pd
+import numpy as np
 import geopandas as gpd
 from dotenv import load_dotenv
 from pathlib import Path
@@ -21,13 +22,18 @@ def intersect_type_check(bf, parcel_gdf):
     '''
     Function to check for type of intersection between building footprints and linking data. Outputs an integer value based on the number of intersections
     '''
-    inter = tuple(map(lambda p: bf.within(p), parcel_gdf['geometry']))
-    inter = [x for x in inter if x == True]
-    print(inter)
-    sys.exit()
-    true_count = sum(inter) # Because True = 1 False = 0
+
+    inter = tuple(map(lambda p: p.intersects(bf), parcel_gdf['geometry']))
+    
+    true_count = sum(inter) # Because True == 1 False == 0    
     return true_count
 
+def intersect_count_flag(intersect_count):
+    '''Returns a flag when more than 1 intersect is detected'''
+    if intersect_count <= 1:
+        return np.Nan
+    if intersect_count > 1:
+        return 1
 
 # -------------------------------------------------------
 # Inputs
@@ -40,6 +46,8 @@ pr_gpkg =  Path(os.getenv('NT_GPKG'))
 ad_lyr_nme = os.getenv('CLEANED_AP_LYR_NAME')
 parcel_lyr_nme = os.getenv('CLEANED_SP_LYR_NAME')
 
+proj_crs = int(os.getenv('NT_PROJ_CRS'))
+
 # -------------------------------------------------------
 # Logic
 
@@ -48,9 +56,15 @@ print('Loading in layers')
 addresses = gpd.read_file(pr_gpkg, layer=ad_lyr_nme, driver='GPKG')
 footprints = gpd.read_file(bf_gpkg, layer=bf_lyr_nme, driver='GPKG')
 parcels = gpd.read_file(pr_gpkg, layer=parcel_lyr_nme,  driver='GPKG')
-print('Running check on intersect counts'//////////////////////////////54)
-linked = footprints[footprints['method'] == 'data_linking']
-linked['intersect_count'] = linked['geometry'].apply(lambda row: intersect_type_check(row, parcels))
-#footprints['multi_intersect_flag'] = 
-print(linked.head())
+
+footprints.to_crs(crs=proj_crs, inplace=True)
+parcels.to_crs(crs=proj_crs, inplace=True)
+addresses.to_crs(crs=proj_crs, inplace=True)
+
+print('Running check on intersect counts')
+
+footprints['intersect_count'] = footprints['geometry'].apply(lambda row: intersect_type_check(row, parcels))
+footprints['multi_intersect_flag'] = footprints['intersect_count'] .apply(lambda row: intersect_type_check(*row))
+
+print(footprints.head())
 print('DONE!')
