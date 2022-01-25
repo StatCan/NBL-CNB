aa671094eed25e9b990ea662b752ab32681c09ed
wenkchr <christopher.wenkoff@statcan.gc.ca>
2022-01-25 14:11:55 -0500

    working version on single layer minor bug identified on multi street name strings

--- scripts/match_accuracy_testing.py
+++ scripts/match_accuracy_testing.py
@@ -42,50 +42,30 @@ that they can be visually inspected to determine logic improvements
 def bf_address_match(address, street_types_dataframe):
     '''Unique to the Fredericton open building data. Convert the address field into its component parts and output as a list in the following order:
     [Address_Min, Address_Max, Street_Name, Street_Typ]'''
-    
+   
     def determine_min_max(address_string):
-        '''
-        returns the min and max values of an input address string in cases where only 1 address is available will return the same number as both the address min and max
-        '''
+        def return_only_numbers(astring):
+            '''Takes a string and returns only numbers if applicable else it returns np.NaN'''
+            stripped = re.sub("[^0-9]", "", astring)
+            if len(address_string) > 0:
+                return stripped
+            else: return np.NaN
 
-        if len(address_string) == 2:
+        fas = address_string[0]
+        output_index = 0
+        if any(char.isdigit() for char in fas):
+            if '-' in fas:
+                fas_split = fas.split('-')
+                fas_split = [return_only_numbers(f) for f in fas_split]
+                return [min(fas_split), max(fas_split), output_index]
+            else:
+                fas = return_only_numbers(fas)
+                if isinstance(fas, str):
+                    return [fas, fas, output_index]
+                else: return [fas, fas, np.NaN]
+                
+        else:
             return [np.NaN, np.NaN, np.NaN]
-
-        # Edge case & at index 1
-        if '&' in address_string:
-            return [address_string[0], address_string[1], 2]
-
-        # If the first item in the list is numeric it is the address number
-        if address_string[0].strip("&AB").isnumeric():
-            a_max = address_string[0].strip("&AB")
-            a_min = address_string[0].strip("&AB")
-            return [a_min, a_max, 0]
-
-        # Records with a range of values in it (format int-int) need to be split and the min max values determined
-        num_split = address_string[0].split('-')
-        if (len(num_split) == 2) and (num_split[0].strip("&AB").isnumeric()) and (num_split[1].strip("&AB").isnumeric()):
-            num_1 = num_split[0].strip("&AB")       
-            num_2  = num_split[1].strip("&AB")
-            if int(num_2) > int(num_1):
-                return [num_1, num_2, 0]
-            else: 
-                return [num_2, num_1, 0]
-        
-        if len(num_split) == 3:
-            # For those cases with three number strings for example (86-92-94 St Marys St) take biggest and smallest #'s middle values will be caught in the range
-            int_list = [int(n) for n in num_split]
-            return [str(max(int_list)), str(min(int_list)), 0]
-
-        if len(num_split) == 1:
-            # cases without #'s longer than 2 items ex. (Carleton Ward (Rear))
-            return [np.NaN, np.NaN, np.NaN]
-
-        if num_split[1] == '1/2':
-            # for that one 325-1/2 case
-            return [num_split[0], num_split[0], 0]
-
-        return [np.NaN, np.NaN, np.NaN] 
-
 
     def get_street_name(address_string):
         '''
@@ -153,6 +133,74 @@ def bf_address_match(address, street_types_dataframe):
     return out_list
 
 
+def abbreviate_street_type(stype, st_type_key_df):
+
+    '''
+    Returns an abbreviated version of the given street name if possible based off the given formatting key in df form
+    '''
+    filtered_key = st_type_key_df[st_type_key_df['Street type'] == stype.upper()]
+    if len(filtered_key) == 1:
+        return filtered_key['Abbreviation'].tolist()[0]
+
+    print(st_type_key_df[st_type_key_df['Street type'] == stype.upper()])
+    print(stype)
+    sys.exit()
+
+
+def match_range_address(bf_index, add_val, bf_df):
+        '''returns match or no match based on input range values in row'''
+        # determine if add value is within range established in the from to list
+        link_row = bf_df.iloc[int(bf_index)]
+        if not isinstance(link_row['address_min'], str):
+            return np.NaN
+        add_rng_max = int(link_row['address_max'])
+        add_rng_min = int(link_row['address_min'])
+        if add_val >= add_rng_min and add_val <= add_rng_max:
+            return True
+        else: return False
+
+def match_street_name(bf_index, str_nme, bf_df):
+    '''returns match or no match between the adp street name and the matched street name in the building footprints'''
+    link_row = bf_df.iloc[int(bf_index)]
+    if not isinstance(link_row['street_name'], str) or not isinstance(str_nme, str):
+            return np.NaN
+
+    if str_nme == link_row['street_name']:
+        return True
+    else:
+        return False
+
+
+def match_street_typ(bf_index, str_typ, bf_df):
+    '''returns match or no match between the adp street type and the matched street type in the building footprints'''
+    link_row = bf_df.iloc[int(bf_index)]
+    if not isinstance(link_row['street_type'], str) or not isinstance(str_typ, str):
+            return np.NaN
+    link_str_type = link_row['street_type'].strip(' ')
+    if str_typ == link_str_type:
+        return True
+    else:
+        # print(str_typ)
+        # print(link_str_type)
+    
+        return False
+
+
+def match_flagger(adr_flag, stn_flag, stt_flag):
+    '''Determines if a match is Full, Partial, or False based on the results from the address, street name,  and street type flag'''
+    # All NaN skip match check
+    if not isinstance(adr_flag, bool) and not isinstance(stn_flag, bool) and not isinstance(stt_flag, bool):
+        return np.NaN
+    match_quality = sum([adr_flag, stn_flag, stt_flag])
+
+    if match_quality == 3:
+        return 'FULL'
+    if (match_quality < 3) and (match_quality > 0):
+        return 'PARTIAL'
+    if match_quality == 0:
+        return 'FALSE'  
+
+
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Inputs
 
@@ -164,6 +212,8 @@ output_path = os.getcwd()
 
 matched_ap_gpkg = Path(os.getenv('MATCHED_OUTPUT_GPKG'))
 
+aoi_mask = Path(os.getenv('AOI_MASK'))
+
 footprint_gpkg = Path(os.getenv('DATA_GPKG'))
 footprints_lyr_nme = os.getenv('CLEANED_BF_LYR_NAME')
 
@@ -174,28 +224,47 @@ str_types_path = Path(os.getenv('RD_TYPES_TXT_PATH'))
 # Address fields for inputs
 bf_address_fields = 'Prop_Loc' # convert to list in future if needed
 
+bf_link_field = 'footprint_index'
+
+m_acc_gpkg = Path(os.getenv('MATCH_ACC_GPKG'))
+
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Logic
 
 # Load in data and prep
 
+aoi_gdf = gpd.read_file(aoi_mask)
+
 str_types_df = pd.read_csv(str_types_path, delimiter='\t')
 str_types_df['Street type'] = str_types_df['Street type'].str.upper()
 
-footprint = gpd.read_file(footprint_gpkg, layer=footprints_lyr_nme, crs=26911)
+footprint = gpd.read_file(footprint_gpkg, layer=footprints_lyr_nme, crs=26911, mask=aoi_gdf)
 footprint.to_crs(crs=proj_crs, inplace=True)
 
-#addresses = gpd.read_file(matched_ap_gpkg, layer='ap_one_to_one_bf_links', driver='GPKG')
-
+addresses = gpd.read_file(matched_ap_gpkg, layer='ap_one_to_one_bf_links', driver='GPKG', mask=aoi_gdf)
+addresses = addresses[~addresses['footprint_index'].isna()]
 # Clean and prep the address data in the inputs
 
-# Footprint address cleaning
+# Footprint address field cleaning
 footprint['address_components'] = footprint[bf_address_fields].apply(lambda bf_address: bf_address_match(bf_address, str_types_df))
 footprint[['address_min', 'address_max', 'street_name', 'street_type']] = pd.DataFrame(footprint['address_components'].tolist(), index= footprint.index)
 footprint.drop(columns=['address_components'], inplace= True)
 
-footprint.to_file()
+# Address points address field cleaning/prep
+addresses['type_en_abv'] = addresses['ST_TYPE_E'].apply(lambda x: abbreviate_street_type(x, str_types_df))
 
-print(footprint.head())
+addresses['ad_rng_check'] = addresses[['footprint_index', 'number']].apply(lambda x: match_range_address(x['footprint_index'], x['number'], footprint), axis=1)
+addresses['str_nme_check'] = addresses[['footprint_index', 'street']].apply(lambda x: match_street_name(x['footprint_index'], x['street'], footprint), axis=1)
+addresses['str_typ_check'] = addresses[['footprint_index', 'type_en_abv']].apply(lambda x: match_street_typ(x['footprint_index'], x['type_en_abv'], footprint), axis=1)
+
+addresses['match_flag'] = addresses.apply(lambda x: match_flagger(x['ad_rng_check'], x['str_nme_check'], x['str_typ_check']), axis=1)
+
+print(addresses[['footprint_index','number', 'street','type_en_abv', 'ad_rng_check', 'str_nme_check', 'str_typ_check', 'match_flag']].head())
+print(footprint.iloc[[1608, 719, 8938, 20477, 2770]])
+
+match_counts = addresses['match_flag'].value_counts()
+print(match_counts)
+for f in ['FULL', 'PARTIAL', 'FALSE']:
+    addresses[addresses['match_flag'] == f].to_file(m_acc_gpkg, layer=f'{f.lower()}_flags', driver='GPKG')
 
 print('DONE!')
