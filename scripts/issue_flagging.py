cfa37cbfa1c4304c23b25df013335ed221e7337a
wenkchr <christopher.wenkoff@statcan.gc.ca>
2022-03-30 11:41:04 -0400

    major changes:
    - changed to a multi processing structure using swifter.
    - culled old code to simplify script

--- scripts/issue_flagging.py
+++ scripts/issue_flagging.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from math import isnan
 import shapely.speedups
 import datetime
-
+import swifter
 shapely.speedups.enable()
 
 '''
@@ -70,9 +70,7 @@ def relationship_setter(parcel_ident, ap_parcel_counts, bf_parcel_counts):
     
     print(ap_count)
     print(bf_count)
-    sys.exit()
-    for rel in ['one_to_one', 'one_to_many', 'many_to_one', 'many_to_many', 'any_to_none']:
-        continue    
+    sys.exit()  
     
 # -------------------------------------------------------
 # Inputs
@@ -102,160 +100,74 @@ ap_cases_gpkg = Path(os.getenv('AP_CASES_GPKG'))
 
 # -------------------------------------------------------
 # Logic
+def main():
+    print('Loading in layers')
+    starttime = datetime.datetime.now()
+    print(f'Start Time {starttime}')
 
-print('Loading in layers')
+    aoi_gdf = None
+    if aoi_mask != None:
+        aoi_gdf = gpd.read_file(aoi_mask)
+    
+    print('Loading in data')
+    addresses = gpd.read_file(ap_path, layer=ap_lyr_nme, mask=aoi_gdf)
+    footprints = gpd.read_file(bf_path, layer=bf_lyr_nme, mask=aoi_gdf)
+    parcels = gpd.read_file(linking_data_path, layer=linking_lyr_nme, mask=aoi_gdf)
+    footprints.to_crs(crs=proj_crs, inplace=True)
+    parcels.to_crs(crs=proj_crs, inplace=True)
+    addresses.to_crs(crs=proj_crs, inplace=True)
 
-starttime = datetime.datetime.now()
-print(f'Start Time {starttime}')
+    metrics = []
+    flags = []
+    print('Producing basic layers')
+    # Linking fields creation
+    addresses["addresses_index"] = addresses.index
+    footprints["footprint_index"] = footprints.index
 
-aoi_gdf = None
-if aoi_mask != None:
-    aoi_gdf = gpd.read_file(aoi_mask)
+    print(f"TOTAL # PARCELS = {len(parcels)}")
+    print(f"TOTAL # ADDRESS POINTS = {len(addresses)}")
+    print(f"TOTAL # BUILDING FOOTPRINTS = {len(footprints)}")
 
-addresses = gpd.read_file(ap_path, layer=ap_lyr_nme, mask=aoi_gdf)
-footprints = gpd.read_file(bf_path, layer=bf_lyr_nme, mask=aoi_gdf)
-parcels = gpd.read_file(linking_data_path, layer=linking_lyr_nme, mask=aoi_gdf)
-footprints.to_crs(crs=proj_crs, inplace=True)
-parcels.to_crs(crs=proj_crs, inplace=True)
-addresses.to_crs(crs=proj_crs, inplace=True)
+    metrics.append(['TOTAL # PARCELS', len(parcels)])
+    metrics.append(['TOTAL # ADDRESS POINTS', len(addresses)])
+    metrics.append(['TOTAL # BUILDING FOOTPRINTS', len(footprints)])
+    # Address Point Metrics
 
-metrics = []
-flags = []
+    # Count of points not in parcels
+    ap_par_len = addresses['link_field'].isna().sum()
 
-# Linking fields creation
-addresses["addresses_index"] = addresses.index
-footprints["footprint_index"] = footprints.index
+    ap_n_pa = addresses.index[addresses['link_field'].isna()].tolist() # Put all the indexes into a list for later
+    print(f"METRIC: POINTS NOT IN PARCEL = {ap_par_len}")
+    metrics.append(['POINTS NOT IN PARCEL', ap_par_len])
 
-print(f"TOTAL # PARCELS = {len(parcels)}")
-print(f"TOTAL # ADDRESS POINTS = {len(addresses)}")
-print(f"TOTAL # BUILDING FOOTPRINTS = {len(footprints)}")
+    no_link = addresses[addresses.Pan_Int.isna()]
 
-metrics.append(['TOTAL # PARCELS', len(parcels)])
-metrics.append(['TOTAL # ADDRESS POINTS', len(addresses)])
-metrics.append(['TOTAL # BUILDING FOOTPRINTS', len(footprints)])
-# Address Point Metrics
+    # Counts of all parcels with more than 1 point
+    linkage_counts = addresses['link_field'].value_counts()
+    linkage_counts = linkage_counts[linkage_counts > 1].index.tolist() # save the indexes for later
+    linkage_len = len(linkage_counts)
+    print(f"METRIC: MORE THAN 1 POINT IN PARCEL = {linkage_len}")
+    metrics.append(['MORE THAN 1 POINT IN PARCEL', linkage_len])
 
-# Count of points not in parcels
-ap_par_len = addresses['link_field'].isna().sum()
+    # Add footprint and address relates by parcel 'groupby'
+    print('Grouping APs')
+    grouped_ap = addresses.groupby('link_field', dropna=True)['link_field'].count()
+    print('Grouping BFs')
+    grouped_bf = footprints.groupby('link_field', dropna=True)['link_field'].count()
+    print('Determining relationship')
+    addresses['parcel_rel'] = addresses['link_field'].swifter.apply(lambda x: relationship_setter(x, grouped_ap, grouped_bf))
 
-ap_n_pa = addresses.index[addresses['link_field'].isna()].tolist() # Put all the indexes into a list for later
-print(f"METRIC: POINTS NOT IN PARCEL = {ap_par_len}")
-metrics.append(['POINTS NOT IN PARCEL', ap_par_len])
+    print('Creating and exporting metrics doc as spreadsheet')
+    metrics_df = pd.DataFrame(metrics, columns=['Metric', 'Count'])
+    metrics_df.to_csv(os.path.join(metrics_out_path, 'Preprocess_Metrics.csv'), index=False)
 
-no_link = addresses[addresses.Pan_Int.isna()]
+    addresses.drop(columns=['addresses_index'], inplace=True)
+    addresses.to_file(output_gpkg, layer=f'ap_full', driver='GPKG')
+    # hour : minute : second : microsecond
+    print(f'Total Runtime: {datetime.datetime.now() - starttime}')
 
-# Counts of all parcels with more than 1 point
-linkage_counts = addresses['link_field'].value_counts()
-linkage_counts = linkage_counts[linkage_counts > 1].index.tolist() # save the indexes for later
-linkage_len = len(linkage_counts)
-print(f"METRIC: MORE THAN 1 POINT IN PARCEL = {linkage_len}")
-metrics.append(['MORE THAN 1 POINT IN PARCEL', linkage_len])
+    print('DONE!')
 
-# Footprint Metrics
-
-# Get a count of all fooprints not in a parcel
-
-# REDO THIS SECTION!
-
-# print(f"METRIC: FOOTPRINT NOT IN A PARCEL = {len(no_intersect)}")
-# metrics.append(['FOOTPRINT NOT IN A PARCEL', len(no_intersect)])
-
-# # Multi Buildings in a singular parcel
-# multi_parcel_int_count = len(footprints.loc[footprints['parcel_count'] > 1])
-
-# print(f"METRIC: MULTI-INTERSECT FOOTPRINTS TO PARCELS =  {multi_parcel_int_count}")
-# metrics.append(['MULTI-INTERSECT FOOTPRINTS TO PARCELS', multi_parcel_int_count])
-
-# Add footprint and address relates by parcel 'groupby'
-grouped_ap = addresses.groupby('link_field', dropna=True)['link_field'].count()
-grouped_bf = footprints.groupby('link_field', dropna=True)['link_field'].count()
-
-addresses['parcel_rel'] = addresses['link_field'].apply(lambda x: relationship_setter(x, grouped_ap, grouped_bf))
-
-# parcels.set_index('Pan_Int', inplace= True)
-
-# Parcel Metrics
-# Join points and footprints to the parcels for counts
-# parcel_join_ap = gpd.sjoin(parcels[['link_field', 'geometry']], addresses[['addresses_index', 'geometry']])
-# parcel_join_bf = gpd.sjoin(parcels[['link_field', 'geometry']], footprints[['footprint_index', 'geometry']])
-
-# p_ap_inter_cnt = parcel_join_ap.index.value_counts()
-# p_ap_inter_cnt.rename('ap_count', inplace=True)
-
-# p_bf_inter_cnt = parcel_join_bf.index.value_counts()
-# p_bf_inter_cnt.rename('bf_count', inplace=True)
-
-# parcels = parcels.merge(p_ap_inter_cnt, left_index= True, right_index=True, how='left')
-# parcels = parcels.merge(p_bf_inter_cnt, left_index= True, right_index=True, how='left')
-# parcels["bf_count"].fillna(0, inplace=True)
-# parcels["ap_count"].fillna(0, inplace=True)
-# parcels["bf_count"] = parcels["bf_count"].astype(int)
-# parcels["ap_count"] = parcels["ap_count"].astype(int)
-
-# m_ap_than_bf_cnt = len(parcels[parcels['bf_count'] < parcels['ap_count']])
-# print(f"METRIC: MORE AP THAN BF IN PARCEL = {m_ap_than_bf_cnt}")
-# metrics.append(['MORE AP THAN BF IN PARCEL', m_ap_than_bf_cnt])
-
-# multi_bf_parcels = len(parcels[parcels['bf_count'] > 1])
-# print(f"METRIC: MULTI FOOTPRINTS IN PARCEL = {multi_bf_parcels}")
-# metrics.append(['MULTI FOOTPRINTS IN PARCEL', multi_bf_parcels])
-
-# multi_ap_parcels = len(parcels[parcels['ap_count'] > 1])
-# print(f"METRIC: MULTI ADDRESS POINTS IN PARCEL = {multi_ap_parcels}")
-# metrics.append(['MULTI ADDRESS POINTS IN PARCEL', multi_ap_parcels])
-
-# # Address point to building footprint relationship counts
-# one_to_one = parcels[(parcels['ap_count'] == 1) & (parcels['bf_count'] == 1)].index.tolist()
-# one_to_many = parcels[(parcels['ap_count'] == 1) & (parcels['bf_count'] > 1)].index.tolist()
-# many_to_one = parcels[(parcels['ap_count'] > 1) & (parcels['bf_count'] == 1)].index.tolist()
-# many_to_many = parcels[(parcels['ap_count'] > 1) & (parcels['bf_count'] > 1)].index.tolist()
-# none_to_any = parcels[(parcels['ap_count'] == 0) & (parcels['bf_count'] > 0)].index.tolist()
-# any_to_none = parcels[(parcels['ap_count'] > 0) & (parcels['bf_count'] == 0)].index.tolist()
-
-# print(f"METRIC: ONE TO ONE ON PARCEL = {len(one_to_one)}")
-# print(f"METRIC: ONE TO MANY ON PARCEL = {len(one_to_many)}")
-# print(f"METRIC: MANY TO ONE ON PARCEL = {len(many_to_one)}")
-# print(f"METRIC: MANY TO MANY ON PARCEL = {len(many_to_many)}")
-# print(f"METRIC: NONE TO ANY ON PARCEL = {len(none_to_any)}")
-# print(f"METRIC: ANY TO NONE ON PARCEL = {len(any_to_none)}")
-
-# metrics.append(['ONE TO ONE ON PARCEL', len(one_to_one)])
-# metrics.append(['ONE TO MANY ON PARCEL', len(one_to_many)])
-# metrics.append(['MANY TO MANY ON PARCEL', len(many_to_one)])
-# metrics.append(['MANY TO MANY ON PARCEL', len(many_to_many)])
-# metrics.append(['NONE TO ANY ON PARCEL', len(none_to_any)])
-# metrics.append(['ANY TO NONE ON PARCEL', len(any_to_none)])
-
-# flags = [['ONE TO ONE', one_to_one], ['ONE TO MANY', one_to_many],['MANY TO ONE', many_to_one], ['MANY TO MANY', many_to_many]]
-
-# parcels.to_file(output_gpkg, layer='parcel_test', driver='GPKG')
-
-print('Creating and exporting metrics doc as spreadsheet')
-metrics_df = pd.DataFrame(metrics, columns=['Metric', 'Count'])
-metrics_df.to_csv(os.path.join(metrics_out_path, 'Preprocess_Metrics.csv'), index=False)
-
-# Test Sorted Exports
-# export_dict = {'one_to_one' : one_to_one,
-#             'one_to_many' : one_to_many,
-#             'many_to_one' : many_to_one,
-#             'many_to_many' : many_to_many,
-#             'none_to_any' : none_to_any,
-#             'any_to_none' : any_to_none}
-
-# no_link.to_file(ap_cases_gpkg, layer='ap_no_link', driver='GPKG')
-# footprints[footprints['Pan_Int'].isin(none_to_any)].to_file(output_gpkg, layer='bf_none_to_any', driver='GPKG')
-
-# addresses['parcel_rel'] = addresses['Pan_Int'].apply(lambda x: relationship_setter(x, export_dict))
-# for l in ['one_to_one', 'one_to_many', 'many_to_one', 'many_to_many', 'any_to_none']:
-#     l_list = export_dict[l]
-#     if len(l_list) == 0: continue
-    # print(addresses[addresses['Pan_Int'].isin(l_list)].head())
-    # addresses[addresses['Pan_Int'].isin(l_list)].to_file(ap_cases_gpkg, layer=f'ap_{l}', driver='GPKG')
-    # footprints.to_file(output_gpkg, layer=f'bf_{l}', driver='GPKG')
-
-addresses.drop(columns=['addresses_index'], inplace=True)
-addresses.to_file(output_gpkg, layer=f'ap_full', driver='GPKG')
-# hour : minute : second : microsecond
-print(f'Total Runtime: {datetime.datetime.now() - starttime}')
-
-print('DONE!')
+if __name__ == "__main__":
+    main()
+    
