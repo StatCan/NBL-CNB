c83fb12e039877edd518b2f3a2633806e8195fa6
wenkchr <christopher.wenkoff@statcan.gc.ca>
2022-05-16 09:52:00 -0400

    fix for no crs for qc outputs

--- scripts/qa_qc.py
+++ scripts/qa_qc.py
@@ -59,12 +59,17 @@ project_gpkg = Path(os.getenv('DATA_GPKG'))
 addresses_lyr_nme = os.getenv('FLAGGED_AP_LYR_NME')
 
 qa_qc_gpkg = Path(os.getenv('QA_GPKG'))
+
 # ----------------------------------------------------------------------------------------------------
 # logic
 
-
+print('Running: qa_qc.py')
 match_adp = gpd.read_file(matched_points_path, layer=matched_points_lyr_nme, crs=proj_crs)
 clean_adp = gpd.read_file(project_gpkg, layer=addresses_lyr_nme, crs=proj_crs)
+print('Creating Line Links')
+
+match_adp.to_crs(crs=proj_crs, inplace=True)
+clean_adp.to_crs(crs=proj_crs, inplace=True)
 
 match_adp['line_geom'] = match_adp[['a_id', 'geometry']].apply(lambda row: point_line_maker(row[1], clean_adp[clean_adp['a_id'] == row[0]]['geometry'].tolist()[0]), axis=1)
 
@@ -76,7 +81,7 @@ match_adp= match_adp.set_geometry('geometry')
 
 # Filter out those links that exceed the maximum limit for linkage distance
 match_adp['link_length'] = match_adp['geometry'].apply(lambda l: l.length)
-
+print('Filtering out long links')
 # Filter out records that are greater than the maximum acceptable link length
 long_links = match_adp.loc[match_adp['link_length'] > max_link_distance]
 print(f'Exporting: {len(long_links)} long links')
@@ -88,7 +93,8 @@ if len(long_links) > 0:
     long_links.drop(columns=['geometry'], inplace=True)
     long_links.rename(columns={'point_geometry':'geometry'}, inplace=True)
     long_links = long_links.set_geometry('geometry')
-    long_links.to_file(qa_qc_gpkg, layer=long_link_point_lyr_nme, driver='GPKG')
+    print('Exporting Long Links')
+    long_links.to_file(qa_qc_gpkg, layer=long_link_point_lyr_nme, driver='GPKG', crs=proj_crs)
 
     # filter out records that are above the link threshold 
     match_adp = match_adp[~match_adp.index.isin((long_links.index))]
@@ -96,7 +102,8 @@ if len(long_links) > 0:
 # Output line file to the qa_qc gpkg
 line_links = match_adp.copy(deep=True)
 line_links.drop(columns=['point_geometry'], inplace=True)
-line_links.to_file(qa_qc_gpkg, layer=f"line_link_{datetime.datetime.now().strftime('%d_%m_%Y')}", driver='GPKG')
+line_links.to_file(qa_qc_gpkg, layer=f"line_link_{datetime.datetime.now().strftime('%d_%m_%Y')}", driver='GPKG', crs=proj_crs)
+print('Exporting qc_points')
 
 match_adp.drop(columns=['geometry'], inplace=True)
 match_adp.rename(columns={'point_geometry':'geometry'}, inplace=True)
